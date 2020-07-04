29de3315730d548dae434cfe240a3beeb7aab51f
Nexhan <subodhkhadka3@gmail.com>
2020-07-03 19:32:20 -0700

    Updates

---
+++ precache-manifest.dc7740524f7ad8d704df56d3ece56236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c606357b128165ec843892e7bd6ef6c",
+    "url": "/Color-boxes/index.html"
+  },
+  {
+    "revision": "cc8de1d91b8844189177",
+    "url": "/Color-boxes/static/css/main.44264e3b.chunk.css"
+  },
+  {
+    "revision": "3c281b430d58ece7e080",
+    "url": "/Color-boxes/static/js/2.7bedb49d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Color-boxes/static/js/2.7bedb49d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8de1d91b8844189177",
+    "url": "/Color-boxes/static/js/main.537d3d62.chunk.js"
+  },
+  {
+    "revision": "c958711dac81a6e18be3",
+    "url": "/Color-boxes/static/js/runtime-main.79b01866.js"
+  }
+]);
